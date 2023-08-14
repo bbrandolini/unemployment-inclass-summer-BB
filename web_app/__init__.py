b4190f7c41523710da66d90e263c7454124042d0
bbrandolini <138704906+bbrandolini@users.noreply.github.com>
2023-08-14 18:32:31 -0400

    Update __init__.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,8 +5,9 @@ from flask import Flask
 from web_app.routes.home_routes import home_routes
 #from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
-from web_app.routes.unemployment_routes import unemployment_routes
-from web_app.routes.stocks_routes import stocks_routes
+#from web_app.routes.unemployment_routes import unemployment_routes
+#from web_app.routes.stocks_routes import stocks_routes
+from web_app.routes.personality_routes import personality_routes
 
 
 def create_app():
